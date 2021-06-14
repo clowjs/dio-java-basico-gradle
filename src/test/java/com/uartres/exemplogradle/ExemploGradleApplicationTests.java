77f684cde535b3d81eff710351fe80a04ed188f0
Sergio Jose Santos Junior <hagare@gmail.com>
2021-06-14 08:18:32 -0300

    Initial commit

---
+++ src/test/java/com/uartres/exemplogradle/ExemploGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uartres.exemplogradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemploGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
